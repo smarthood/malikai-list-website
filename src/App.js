f3cb564fc37efa1bf8a7a946f7c1e0ef006d27ae
smarthood <renolddickson18@gmail.com>
2022-06-26 10:33:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Data from './Data'
 
 function App() {
+  const [item,setItem]=useState(Data);
+  const [keyword,setKeyword] = useState("");
+  const handleOnChange = (e)=>{
+    setKeyword(e.target.value);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="searchbar">
+      <input type="text" placeholder='search items' onChange={handleOnChange}/>
+      <button className='searchbtn'>Search</button>
+      </div>
+      <h2>No of items: {item.length}</h2>
+      <div className="heading">
+        <span className="1">Product</span>
+        <span className="2">Quantity</span>
+        <span className="3">Price</span>
+      </div>
+      {item.filter((items)=>{
+        if(keyword === ""){
+          return items
+        }
+        else if (items.name.toLowerCase().includes(keyword.toLowerCase())){
+          return items
+        }
+      }).map((items,key)=>{
+        const {name,id,quantity,price}=items;
+        return (
+            <div className="container" key={id}>
+               <span className="Name">{name}</span>
+               <span className="quantity">{quantity}</span>
+               <span className="price">₹{price}</span>
+            </div>
+        )
+    })}
     </div>
   );
 }
---
+++ src/Data.js
@@ -0,0 +1,74 @@
+export default [
+    {
+        id: 1,
+        name: "tomato",
+        quantity: "100g",
+        price: 5
+    },
+    {
+        id: 2,
+        name: "carrot",
+        quantity: "100g",
+        price: 5
+    },
+    {
+        id: 3,
+        name: "onion",
+        quantity: "100g",
+        price: 4
+    },
+    {
+        id: 4,
+        name: "Egg",
+        quantity: 1,
+        price: 6
+    },
+    {
+        id: 5,
+        name: "Rasna",
+        quantity: 1,
+        price: 5
+    },
+    {
+        id: 6,
+        name: "Maggi",
+        quantity: 1,
+        price: 12
+    },
+    {
+        id: 7,
+        name: "Mixture",
+        quantity: 1,
+        price: 34
+    },
+    {
+        id: 8,
+        name: "chevu",
+        quantity: 1,
+        price: 34
+    },
+    {
+        id: 9,
+        name: "varuval",
+        quantity: 1,
+        price: 34
+    },
+    {
+        id: 10,
+        name: "Floor",
+        quantity: 1,
+        price: 32
+    },
+    {
+        id: 11,
+        name: "coconut",
+        quantity: "100g",
+        price: 32
+    },
+    {
+        id: 12,
+        name: "palm oil",
+        quantity: "1/4l",
+        price: 32
+    },  
+];
